0f2903e18cf8c417890502aeb4889df0c51280cc
mark-brongersma <mark@twokings.nl>
2023-06-27 14:25:53 +0200

    test

---
+++ src/theme/test/disablescroll.js
@@ -0,0 +1,10 @@
+function disableScroll() { 
+    document.body.classList.add("remove-scrolling"); 
+  } 
+  
+  function enableScroll() { 
+    document.body.classList.remove("remove-scrolling"); 
+  }
+
+  
+  // https://codepen.io/atiqbd/pen/jOEbVKZ
